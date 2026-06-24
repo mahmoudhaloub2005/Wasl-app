70f9385802625f7b58493f2081512c470110e0d9
callink97 <aymyn.mk@gmail.com>
2026-06-24 10:47:08 +0300

    last commit

---
+++ src/components/Generators/Generators.jsx
@@ -0,0 +1,78 @@
+import DashboardNavbar from "../DashboardNavbar/DashboardNavbar";
+import FilterSection from "../FilterSection/FilterSection";
+import GeneratorCard from "../GeneratorCard/GeneratorCard";
+import Footer from "../Footer/Footer";
+
+import generator1 from "../../assets/images/generator1.jpg";
+import generator2 from "../../assets/images/generator2.jpg";
+import generator3 from "../../assets/images/generator3.jpg";
+
+import "./Generators.css";
+
+function Generators() {
+  return (
+    <>
+      <DashboardNavbar />
+
+      <section className="generators-page">
+
+        <div className="page-header">
+
+          <div>
+
+            <h1>المولدات</h1>
+
+            <p>
+              استعرض المولدات المتوفرة في منطقتك وقارن الأسعار والخدمات.
+            </p>
+
+          </div>
+
+          <button className="compare-btn">
+            قارن
+          </button>
+
+        </div>
+
+        <FilterSection />
+
+        <div className="generators-grid">
+
+          <GeneratorCard
+            image={generator1}
+            name="مولد الوافدين"
+            location="دير البلح"
+            price="₪18,500"
+            capacity="450 أمبير"
+            status="تحت الصيانة"
+          />
+
+          <GeneratorCard
+            image={generator2}
+            name="مولد الرشيد الذكي"
+            location="دير البلح"
+            price="₪18,500"
+            capacity="450 أمبير"
+            status="يعمل الآن"
+          />
+
+          <GeneratorCard
+            image={generator3}
+            name="مولد النور"
+            location="دير البلح"
+            price="₪25,000"
+            capacity="450 أمبير"
+            status="يعمل الآن"
+          />
+
+        </div>
+
+      </section>
+
+      <Footer />
+    </>
+  );
+}
+
+export default Generators;
+
